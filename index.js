5465187e421d9e3c446c796ae13392ddbf35c35e
husky77 <58156291+husky77@users.noreply.github.com>
2025-12-12 17:49:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const jsonfile = require("jsonfile");
-const moment = require("moment");
-const simpleGit = require("simple-git");
-const random = require("random");
+import jsonfile from "jsonfile";
+import moment from "moment";
+import simpleGit from "simple-git";
+import random from "random";
 
 const path = "./data.json";
 function getRndInteger(min, max) {
@@ -14,13 +14,12 @@ let counter = 0;
 const makeCommits = (n) => {
   if (n === 0) return4
   counter++;
-  console.log(Commit #${counter});
+  console.log(`Commit #${counter}`);
   
-  const x = Math.floor(Math.random() * (54 + 1)); // Generates a random integer between 0 and 54
-  const y = Math.floor(Math.random() * (5 + 1));  // Generates a random integer between 0 and 5
-
+  const x = random.int(0, 54);
+  const y = random.int(0, 5);
   const date = moment()
-    .subtract(getRndInteger(0,4), "y")
+    .subtract(getRndInteger(0,6), "y")
     .add(1, "d")
     .add(x, "w")
     .add(y, "d")
@@ -50,4 +49,4 @@ const makeCommits = (n) => {
   });
 };
 
-makeCommits(2340);
+makeCommits(900);
